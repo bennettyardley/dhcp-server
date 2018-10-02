7a8d12f34029a13982e78a4a838cc4e2ba62319d
Bennett Yardley <33298178+bennettyardley@users.noreply.github.com>
2018-10-02 10:57:56 -0400

    Add files via upload

---
+++ dhcp.py
@@ -0,0 +1,9 @@
+from scapy.all import *
+import pydivert
+
+#https://en.wikipedia.org/wiki/Dynamic_Host_Configuration_Protocol
+
+with pydivert.WinDivert("tcp.DstPort == 67") as syn:
+    for packet in syn:
+        syn.send(packet)
+        sync(packet, localIP)
