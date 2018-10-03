fe5cf24d4eb7e7ad02b5fadf3cb3f24011f8f44e
Bennett Yardley <33298178+bennettyardley@users.noreply.github.com>
2018-10-03 16:33:15 -0400

    Update dhcp.py

--- dhcp.py
+++ dhcp.py
@@ -3,7 +3,7 @@ import pydivert
 
 #https://en.wikipedia.org/wiki/Dynamic_Host_Configuration_Protocol
 
- pydivert.WinDivert("tcp.DstPort == 67") as syn:
+with pydivert.WinDivert("tcp.DstPort == 67") as syn:
     for packet in syn:
         syn.send(packet)
         sync(packet, localIP)
