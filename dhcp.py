ec4fc49627bbc10351c113408075aee138ad96b7
Bennett Yardley <33298178+bennettyardley@users.noreply.github.com>
2018-10-10 08:02:47 -0400

    Update dhcp.py

--- dhcp.py
+++ dhcp.py
@@ -8,7 +8,8 @@ def sync(packet):
     #craft DHCP offer 
     #send DHCP offer
     #wait for request
-    
+    #send ack
+    #add taken ip to list
 
 with pydivert.WinDivert("tcp.DstPort == 67") as syn:
     for packet in syn:
