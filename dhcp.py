13b4e13e266d5857706f83c0559ec43f3b32c3af
Bennett Yardley <33298178+bennettyardley@users.noreply.github.com>
2018-10-10 08:03:11 -0400

    Update dhcp.py

--- dhcp.py
+++ dhcp.py
@@ -1,5 +1,3 @@
-#https://en.wikipedia.org/wiki/Dynamic_Host_Configuration_Protocol
-
 from scapy.all import *
 import pydivert
 
@@ -11,6 +9,7 @@ def sync(packet):
     #send ack
     #add taken ip to list
 
+ 
 with pydivert.WinDivert("tcp.DstPort == 67") as syn:
     for packet in syn:
         syn.send(packet)
